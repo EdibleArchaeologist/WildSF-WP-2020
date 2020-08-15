ff8d5545a7f61d360ca0af8fa22df2306e5c60c7
Kelley Shanahan <shanahangirl@gmail.com>
2020-08-15 15:07:48 -0500

    First Round Navbar changes

--- navbar.php
+++ navbar.php
@@ -16,68 +16,78 @@ $logo_image_url = wp_get_attachment_image_url(
 	'thumbnail-no-crop');
 ?>
 
-<nav class="navbar navbar-expand-lg navbar-dark fixed-top">
-	<a class="navbar-brand" href="<?= is_front_page()?'#':$frontpage_url ?>">
-		<img src="<?= $logo_image_url ?>">
-	</a>
+<nav class="navbar navbar-expand-lg fixed-top">
+	<!-- Centered link -->
+  <div class="topnav-centered">
+		<a class="navbar-brand" href="<?= is_front_page()?'#':$frontpage_url ?>">
+			<img src="<?= $logo_image_url ?>">
+		</a>
+  </div>
+
 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 		<span class="navbar-toggler-icon"></span>
 	</button>
 
-	<div class="collapse navbar-collapse" id="navbarSupportedContent">
-		<ul class="navbar-nav mr-auto">
-			<li class="nav-item dropdown">
-				<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-				Tours
-				</a>
-				<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-					<?php
-					$args = array( 'post_type' => 'tour',
-						'meta_query' => array(
-							array(
-								'key' => 'active_tour',
-								'compare' => '=',
-								'value' => '1'
+	<div class="collapse navbar-collapse container-fluid" id="navbarSupportedContent">
+		<div class="row menu-items justify-content-between">
+			<!-- Left-aligned links (default) -->
+			<ul class="navbar-nav topnav-left col-6">
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					Tours
+					</a>
+					<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+						<?php
+						$args = array( 'post_type' => 'tour',
+							'meta_query' => array(
+								array(
+									'key' => 'active_tour',
+									'compare' => '=',
+									'value' => '1'
+								)
 							)
-						)
-				 	);
-					$loop = new WP_Query( $args );
-					while ( $loop->have_posts() ) : $loop->the_post();
-					?>
-					<div data-toggle="collapse" data-target=".navbar-collapse.show" >
-						<a class="dropdown-item" href="<?= get_permalink($post) ?>">
-							<span class="title"><?= the_title() ?></span>
-						</a>
-					</div>
+					 	);
+						$loop = new WP_Query( $args );
+						while ( $loop->have_posts() ) : $loop->the_post();
+						?>
+						<div data-toggle="collapse" data-target=".navbar-collapse.show" >
+							<a class="dropdown-item" href="<?= get_permalink($post) ?>">
+								<span class="title"><?= the_title() ?></span>
+							</a>
+						</div>
 
-					<?php endwhile; ?>
-				</div>
-			</li>
-			<!-- <li data-toggle="collapse" data-target=".navbar-collapse.show" class="nav-item">
-				<a href="<?= $calendar_nav_link ?>" class="nav-link" data-button-text="Calendar">Book Now</a>
-			</li> -->
-			<li data-toggle="collapse" data-target=".navbar-collapse.show" class="nav-item">
-				<a class="nav-link" href="<?= $vtb_url = get_permalink( $vtb_page ); ?>">Virtual Team Building</a>
-			</li>
-			<li data-toggle="collapse" data-target=".navbar-collapse.show" class="nav-item">
-				<a class="nav-link" href="<?= $private_tours_url ?>">Private Tours</a>
-			</li>
-			<!-- <li data-toggle="collapse" data-target=".navbar-collapse.show" class="nav-item">
-				<a class="nav-link" href="<?= $faq_nav_link ?>">FAQ</a>
-			</li> -->
-			<!-- <li data-toggle="collapse" data-target=".navbar-collapse.show" class="nav-item nav-gift-card">
-				<a href="https://www.peek.com/s/15a8284c-0990-4986-a5b4-1754b0c0b014/K9Wd" class="nav-link" data-button-text="Gift Card">Gift Card</a>
-			</li> -->
-			<li data-toggle="collapse" data-target=".navbar-collapse.show" class="nav-item">
-				<a class="nav-link" href="/about-us">About Us</a>
-			</li>
-			<li data-toggle="collapse" data-target="" class="nav-item">
-				<a href="<?= $book_nav_link ?>" class="btn book-button" data-button-text="Book Now">Book Now</a>
-			</li>
-		</ul>
-		<span class="navbar-text contact-info">
-	      <a href="mailto:info@wildsftours.com" class="email-address">info@wildsftours.com</a>
-		  <a href="tel:+14153906635" class="phone-number">(415)390-6635‬</a>
-	    </span>
+						<?php endwhile; ?>
+					</div>
+				</li>
+				<!-- <li data-toggle="collapse" data-target=".navbar-collapse.show" class="nav-item">
+					<a href="<?= $calendar_nav_link ?>" class="nav-link" data-button-text="Calendar">Book Now</a>
+				</li> -->
+				<li data-toggle="collapse" data-target=".navbar-collapse.show" class="nav-item">
+					<a class="nav-link" href="<?= $vtb_url = get_permalink( $vtb_page ); ?>">Virtual Team Building</a>
+				</li>
+				<li data-toggle="collapse" data-target=".navbar-collapse.show" class="nav-item">
+					<a class="nav-link" href="<?= $private_tours_url ?>">Private Tours</a>
+				</li>
+				<!-- <li data-toggle="collapse" data-target=".navbar-collapse.show" class="nav-item">
+					<a class="nav-link" href="<?= $faq_nav_link ?>">FAQ</a>
+				</li> -->
+				<!-- <li data-toggle="collapse" data-target=".navbar-collapse.show" class="nav-item nav-gift-card">
+					<a href="https://www.peek.com/s/15a8284c-0990-4986-a5b4-1754b0c0b014/K9Wd" class="nav-link" data-button-text="Gift Card">Gift Card</a>
+				</li> -->
+				<li data-toggle="collapse" data-target=".navbar-collapse.show" class="nav-item">
+					<a class="nav-link" href="/about-us">About Us</a>
+				</li>
+			</ul>
+			<!-- Right-aligned links -->
+				<ul class="navbar-nav topnav-right col-5">
+					<li class="nav-item">
+			    	<a class="nav-link" href="mailto:info@wildsftours.com" class="email-address">info@wildsftours.com</a>
+					</li>
+					<li class="nav-item">
+					  <a class="nav-link" href="tel:+14153906635" class="phone-number">(415)390-6635‬</a>
+					</li>
+					<li><a href="<?= $book_nav_link ?>" class="btn book-button" data-button-text="Book Now">Book Now</a></li>
+				</ul>
+		</div>
 	</div>
 </nav>
--- ui.js
+++ ui.js
@@ -1,13 +1,13 @@
 $(()=>{
 
-	//trigger navbar fade when user scrolls down
+	//trigger navbar narrow when user scrolls down
 	let last_scroll_top = 0
 	$(window).scroll(function(){
 		let scroll_top = $(this).scrollTop()
 		if(scroll_top > last_scroll_top){
-			$(".navbar").addClass('fade-away')
+			$(".navbar").addClass('shrink')
 		} else if(scroll_top < last_scroll_top - 10) {
-			$(".navbar").removeClass('fade-away')
+			$(".navbar").removeClass('shrink')
 		}
 		last_scroll_top = scroll_top
 	})
